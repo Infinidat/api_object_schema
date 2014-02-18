da2d65755b83c6c413cefd4c3c06fc31e6dd6ebd
Ayala Shachar <shachar.ayala@gmail.com>
2014-02-18 11:51:15 +0200

    Added bindings

--- api_object_schema/binding.py
+++ api_object_schema/binding.py
@@ -40,3 +40,36 @@ class MethodBinding(FieldBinding):
     def set_object_value_from_api(self, obj, api_value):
         setter_method_name = self._setter_method_name or "set_{0}".format(self._field.name)
         return getattr(obj, setter_method_name)(api_value)
+
+class FunctionBinding(FieldBinding):
+    def __init__(self, get_func=None, set_func=None):
+        self._get_func = get_func
+        self._set_func = set_func
+
+    def get_api_value_from_object(self, obj):
+        if self._get_func:
+            return self._get_func(obj)
+        raise NotImplementedError() # pragma: no cover
+
+    def set_object_value_from_api(self, obj, api_value):
+        if self._set_func:
+            return self._set_func(obj, api_value)
+        raise NotImplementedError() # pragma: no cover
+
+class EmptyBinding(FieldBinding):
+    def get_api_value_from_object(self, obj):
+        return self._field.type.type()
+
+class ConstBinding(FieldBinding):
+    def __init__(self, value):
+        self._value = value
+
+    def get_api_value_from_object(self, obj):
+        return self._value
+
+class CountBinding(FieldBinding):
+    def __init__(self, list_to_sum_name):
+        self._list_name = list_to_sum_name
+
+    def get_api_value_from_object(self, obj):
+        return len(getattr(obj, self._list_name))
