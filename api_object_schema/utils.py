3d9fc2561ee89bff293e4d2be4de2805e306bf88
Rotem Yaari <vmalloc@gmail.com>
2014-06-30 16:41:55 +0300

    Fix Python 3 support

--- api_object_schema/utils.py
+++ api_object_schema/utils.py
@@ -1,13 +1,15 @@
+from ._compat import PY2
+
 def loose_isinstance(obj, type_):
     """
     Like isinstance, only handles ridiculous cases like isinstance(True, int) and isinstance(u"hello", str)
     """
-    if type_ is int or type_ is long:
+    if type_ is int or (PY2 and type_ is long):
         if isinstance(obj, bool):
             return False
-        elif isinstance(obj, (int, long)):
+        elif isinstance(obj, (int, long) if PY2 else int):
             return True
         # fallthrough
-    elif type_ is str and isinstance(obj, unicode):
+    elif type_ is str and PY2 and isinstance(obj, unicode):
         return True
     return isinstance(obj, type_)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,21 @@
+from api_object_schema._compat import PY2
 from api_object_schema.utils import loose_isinstance
 
+_INT_VALUES = [100, 183874837847398473]
+if PY2:
+    _INT_VALUES.append(long(100))
+
+_COMBINATIONS = [
+    (str, ["hello", u"hello"], [1, True, None]),
+    (bool, [True, False], ["s", 1.0, 1]),
+    (int, _INT_VALUES, [1.2, "hello", True, False]),
+]
+
+if PY2:
+    _COMBINATIONS.append((long, _INT_VALUES, [1.2, "hello", True, False]))
+
 def test_loose_isinstance():
-    for type, matching, not_matching in [
-            (str, ["hello", u"hello"], [1, True, None]),
-            (bool, [True, False], ["s", 1.0, 1]),
-            (int, [100L, 100, 183874837847398473], [1.2, "hello", True, False]),
-            (long, [100L, 100, 183874837847398473], [1.2, "hello", True, False]),
-            ]:
+    for type, matching, not_matching in _COMBINATIONS:
         for m in matching:
             assert loose_isinstance(m, type)
         for m in not_matching:
